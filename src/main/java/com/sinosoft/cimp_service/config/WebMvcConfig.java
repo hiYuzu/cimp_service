446c7b796cd9dd1048ffa149ec1d76ce4922094a
hiYuzu <pomelo.yuzu1997@gmail.com>
2021-10-08 14:46:05 +0800

    del test and auto-mvn

--- src/main/java/com/sinosoft/cimp_service/config/WebMvcConfig.java
+++ src/main/java/com/sinosoft/cimp_service/config/WebMvcConfig.java
@@ -41,7 +41,6 @@ public class WebMvcConfig implements WebMvcConfigurer {
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
         registry.addInterceptor(new MyInterceptor())
-                .addPathPatterns("/**")
-                .excludePathPatterns("/test");
+                .addPathPatterns("/**");
     }
 }
