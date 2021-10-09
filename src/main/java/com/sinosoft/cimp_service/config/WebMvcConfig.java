230c624bb19c2fd7f1c644e7db99db4c0d9deb29
hiYuzu <pomelo.yuzu1997@gmail.com>
2021-10-09 14:57:28 +0800

    修复数据查询的错误

--- src/main/java/com/sinosoft/cimp_service/config/WebMvcConfig.java
+++ src/main/java/com/sinosoft/cimp_service/config/WebMvcConfig.java
@@ -5,11 +5,13 @@ import com.alibaba.fastjson.support.config.FastJsonConfig;
 import com.alibaba.fastjson.support.spring.FastJsonHttpMessageConverter;
 import com.sinosoft.cimp_service.config.interceptor.MyInterceptor;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.MediaType;
 import org.springframework.http.converter.HttpMessageConverter;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import java.nio.charset.StandardCharsets;
+import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -24,6 +26,7 @@ public class WebMvcConfig implements WebMvcConfigurer {
     public void configureMessageConverters(List<HttpMessageConverter<?>> converters) {
         // 自定义 FastJson 配置
         FastJsonHttpMessageConverter converter = new FastJsonHttpMessageConverter();
+
         FastJsonConfig config = new FastJsonConfig();
         config.setDateFormat("yyyy-MM-dd HH:mm:ss");
         config.setCharset(StandardCharsets.UTF_8);
@@ -34,7 +37,16 @@ public class WebMvcConfig implements WebMvcConfigurer {
                 SerializerFeature.WriteNullListAsEmpty,
                 SerializerFeature.WriteNullStringAsEmpty
         );
+
+        List<MediaType> mediaTypes = new ArrayList<>();
+        mediaTypes.add(MediaType.APPLICATION_ATOM_XML);
+        mediaTypes.add(MediaType.APPLICATION_CBOR);
+        mediaTypes.add(MediaType.APPLICATION_FORM_URLENCODED);
+        mediaTypes.add(MediaType.APPLICATION_JSON);
+        mediaTypes.add(MediaType.APPLICATION_OCTET_STREAM);
+
         converter.setFastJsonConfig(config);
+        converter.setSupportedMediaTypes(mediaTypes);
         converters.add(converter);
     }
 
